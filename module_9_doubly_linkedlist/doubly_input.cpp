acbd74c8765e6e24c58b89840ea7ba0e195f22d4
Source-Code-007 <utsho926@gmail.com>
2025-07-22 00:04:37 +0600

    doubly linked list completed with all operation with time complexity

---
+++ module_9_doubly_linkedlist/doubly_input.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>;
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *prev;
+    Node *next;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->prev = NULL;
+        this->next = NULL;
+    }
+};
+
+void printForward(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->val << " ";
+        temp = temp->next;
+    }
+}
+
+void insert_at_tail(Node *&head, Node *&tail, int val)
+{
+
+    Node *newNode = new Node(val);
+    if (tail == NULL || head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+
+    tail->next = newNode;
+    newNode->prev = tail;
+    tail = newNode;
+}
+
+int main()
+{
+    Node *head = NULL;
+    Node *tail = NULL;
+
+    while (true)
+    {
+        int num;
+        cin >> num;
+        if (num == -1)
+        {
+            break;
+        }
+        insert_at_tail(head, tail, num);
+    }
+
+    // printForward(head);
+    printForward(head);
+
+    return 0;
+}
