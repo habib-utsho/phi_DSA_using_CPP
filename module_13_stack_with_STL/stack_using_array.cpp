817041a09c2b9c32b526f91cd1b5137796aa9240
Source-Code-007 <utsho926@gmail.com>
2025-08-02 23:23:22 +0600

    stack implementation added

---
+++ module_13_stack_with_STL/stack_using_array.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>;
+using namespace std;
+
+class myStack
+{
+public:
+    vector<int> v;
+
+    void push(int val)
+    {
+        v.push_back(val);
+    };
+    void pop()
+    {
+        v.pop_back();
+    };
+    int top()
+    {
+        return v.back();
+    };
+    int size()
+    {
+        return v.size();
+    };
+    bool empty()
+    {
+        return v.empty();
+    };
+};
+
+main()
+{
+    // myStack *st = new myStack(); // dynamic
+    myStack st; // static
+    // st->push(10); // dynamic
+    // st.pop();
+
+    while (true)
+    {
+        int val;
+        cin >> val;
+        if (val == -1)
+        {
+            break;
+        }
+         st.push(val);
+    }
+
+    if (!st.empty())
+        st.pop();
+    if (!st.empty())
+        st.pop();
+    // st.pop();
+    if (!st.empty())
+        cout << st.top() << endl;
+    cout << st.empty() << endl;
+
+    return 0;
+}
